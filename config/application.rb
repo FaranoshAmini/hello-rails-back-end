c4c456662345706f9d7646d9704d5fbf2ed1c28e
Faranosh Amini <faranosh.amini4@gmail.com>
2023-11-12 01:32:02 +0430

    Update the gemfile

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,14 @@ module HelloRailsBackEnd
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:1234'
+        resource '*',
+        headers: :any,
+        method: :get
+      end
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
